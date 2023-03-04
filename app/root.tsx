254ad3cb411cfc388df1e83be3cee91ea16f4c23
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2023-03-04 16:46:48 -0300

    Initial Config (#1)

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node'
 import {
   Links,
   LiveReload,
@@ -6,14 +6,14 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react'
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "Remix Stack",
-  description: "Remix Stack",
-  viewport: "width=device-width,initial-scale=1",
-});
+  charset: 'utf-8',
+  title: 'Remix Stack',
+  description: 'Remix Stack',
+  viewport: 'width=device-width,initial-scale=1',
+})
 
 export default function App() {
   return (
@@ -29,5 +29,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
