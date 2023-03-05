0c8989f287181331b5239bb845f01bc824a4ec70
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2023-03-05 17:52:03 -0300

    Email server (#7)

---
+++ app/utils/email.server.ts
@@ -0,0 +1,34 @@
+import { getRequiredServerEnvVar } from './misc'
+
+const MAILGUN_SENDING_KEY = getRequiredServerEnvVar('MAILGUN_SENDING_KEY')
+const MAILGUN_DOMAIN = getRequiredServerEnvVar('MAILGUN_DOMAIN')
+
+export async function sendEmail({
+  to,
+  subject,
+  html,
+  text,
+}: {
+  to: string
+  subject: string
+  html: string
+  text: string
+}) {
+  const auth = `${Buffer.from(`api:${MAILGUN_SENDING_KEY}`).toString('base64')}`
+
+  const body = new URLSearchParams({
+    to,
+    from: 'hello@example.com',
+    subject,
+    text,
+    html,
+  })
+
+  return fetch(`https://api.mailgun.net/v3/${MAILGUN_DOMAIN}/messages`, {
+    method: 'post',
+    body,
+    headers: {
+      Authorization: `Basic ${auth}`,
+    },
+  })
+}
